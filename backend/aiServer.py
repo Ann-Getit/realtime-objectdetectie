0aea9fae31a5557b50149dd36c2a62877e3e326f
Ann-Getit <Ann-Getit+182119360+Ann-Getit@users.noreply.github.com>
2025-11-09 20:21:26 +0100

    update backend scripts voor deploy

---
+++ backend/aiServer.py
@@ -0,0 +1,116 @@
+# ai_server.py
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from ultralytics import YOLO
+from PIL import Image
+import io
+import base64
+import numpy as np
+import torch
+import os
+
+app = Flask(__name__)
+
+MODEL_PATH = os.getenv("MODEL_PATH", "backend/weights/best.pt")
+DEVICE = os.getenv("DEVICE", "cpu")
+CONFIDENCE_THRESHOLD = float(os.getenv("CONFIDENCE_THRESHOLD", 0.5))
+
+print("MODEL_PATH:", MODEL_PATH)
+print("DEVICE:", DEVICE)
+print("CONFIDENCE_THRESHOLD:", CONFIDENCE_THRESHOLD)
+
+
+# Alleen requests vanaf http://localhost:3000 toestaan
+CORS(app, resources={r"/*": {"origins": [
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+    "https://ann-getit.github.io/realtime-objectdetectie/"
+    ]}})  #HIER LATER DE FRONTEND URL INVOEREN---------------
+
+
+
+#"http://localhost:3000"
+
+ 
+# Gebruik GPU/Metal als beschikbaar
+device = "cuda" if torch.cuda.is_available() else DEVICE
+
+
+# YOLO-model laden
+model = YOLO(MODEL_PATH)
+model.to(device)
+print("Device in gebruik:", device)
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    try:
+        data = request.get_json()
+        img_base64 = data.get("image")
+        if not img_base64:
+            return jsonify({"error": "Geen 'image' veld"}), 400
+    
+        # ---- Alleen voor testen (tijdelijk) ----
+        # print("Ontvangen afbeelding:", len(img_base64))
+        # return jsonify({"detections": []})
+        
+
+    # Base64 → PIL → numpy
+        img_bytes = base64.b64decode(img_base64)
+
+        try:
+             img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
+        except Exception:
+            return jsonify({"error": "Kon afbeelding niet openen"}), 400
+
+
+        frame = np.array(img)
+
+        print("Frame shape:", frame.shape)
+
+  # YOLO detectie
+        with torch.no_grad():  # geen gradients voor inferentie
+            # In ai_server.py
+            results = model(frame, device=device, imgsz=384)  # laat model zelf resize doen
+
+   
+        # Resultaten parsen
+            detections = []
+            detected_classes = []
+
+            for r in results:
+                for box in r.boxes:
+                    class_name = model.names[int(box.cls)]
+                    if class_name == "not_apple":  # filter
+                       continue
+
+        # Voeg alleen de gefilterde classes toe
+                    detected_classes.append(class_name)
+                    detections.append({
+                        "class": class_name,
+                        "confidence": float(box.conf),
+                        "bbox": box.xyxy[0].tolist()
+                     })
+
+            app.logger.info(f"Frame shape: {frame.shape},  Detecties: {len(detections)}, Classes: {detected_classes}")
+        
+        print("YOLO intern shape:", results[0].path, results[0].orig_shape, results[0].boxes.shape)
+        print(model.names)
+
+
+
+         # VRAM cleanup voor M1 GPU
+        if device == "mps":
+            torch.mps.empty_cache()
+
+    
+        return jsonify({"detections": detections})
+    
+    except Exception as e:
+        # Zorg dat de server niet crasht bij fouten
+        return jsonify({"error": "Flask server error", "details": str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000)) # 5000 is alleen fallback voor lokaal testen 
+    app.run(host="0.0.0.0", port=port)
+
+
