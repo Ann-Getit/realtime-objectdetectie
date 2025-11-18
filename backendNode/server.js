bf30331a0a7ebabc40c44677cda4ed6d96ec9efe
Ann-Getit <Ann-Getit+182119360+Ann-Getit@users.noreply.github.com>
2025-11-18 14:23:30 +0100

    changes to connect both live webservices

---
+++ backendNode/server.js
@@ -0,0 +1,69 @@
+// server.js
+const express = require('express');
+const cors = require('cors'); // cors is een browserbeveiliging
+const app = express();
+const port = process.env.PORT || 3000;
+//const path = require('path');
+
+
+// Voor JSON body (base64 frame)
+app.use(express.json({ limit: '10mb' }));
+
+
+app.use(cors({ 
+    origin: ['http://127.0.0.1:5500', 'http://127.0.0.1:5050', 'https://ann-getit.github.io/realtime-objectdetectie/'], //frontend pages url moet hier 
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+
+
+
+
+
+
+//app.use(express.static(path.join(__dirname, 'public'))); alleen voor lokaal gebruik
+
+
+
+
+app.post('/detect', async (req, res) => {
+    try {
+        const imageBase64 = req.body.image;  // base64 string
+        if (!imageBase64) return res.status(400).json({ error: 'No image provided' });
+   
+ 
+        // Stuur frame naar Flask server
+        const flaskResponse = await fetch('https://backend-realtime-ai.onrender.com/detect', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ image: imageBase64 })
+        });
+
+        const text = await flaskResponse.text(); // <-- lees de tekst terug van Flask
+
+        if (!flaskResponse.ok) {
+            return res.status(500).json({ error: 'Flask server error', details: text });
+        }
+
+          // ✅ geef de Flask-response terug aan de frontend
+          res.type('application/json').send(text);
+
+    } catch (err) {
+        console.error('Server error:', err);
+        res.status(500).json({ error: 'Server error', details: err.message });
+    }
+});
+
+//app.get('/', (req, res) => {
+  //res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//});
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+
+
+
+app.listen(port, '0.0.0.0', () => console.log(`✅ Node.js server listening on ${port}🚀`));
+
